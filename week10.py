857662209da3976d44abe9d7ba374c82b0d99c8b
TadBak <35119592+TadBak@users.noreply.github.com>
2018-03-07 17:48:10 +1100

    Update week10.py

--- week10.py
+++ week10.py
@@ -1,6 +1,5 @@
 def multiziperator(*args):
-    generators = [(item for item in iterator) for iterator in args]
-   
+    generators = [(item for item in iterator) for iterator in args
     while True:
         for gen in generators:
             yield next(gen)
