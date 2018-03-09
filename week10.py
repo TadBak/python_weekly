5e25e4e8774755ced1eeb2b395337e99485f398e
TadBak <35119592+TadBak@users.noreply.github.com>
2018-03-09 12:14:28 +1100

    Add files via upload
    There are two versions of multiziperator now

--- week10.py
+++ week10.py
@@ -1,8 +1,22 @@
 def multiziperator(*args):
+    """ Returns interleaved elements from all input iterators until the shortest
+        one is exhausted, consequently the number of returned elements coming
+        from different iterators can not be the same.
+    """
     generators = [(item for item in iterator) for iterator in args]
     while True:
         for gen in generators:
             yield next(gen)
 
 
+def multiziperator(*args):
+    """ Returns interleaved elements from all input iterators in such a way that
+        the number of returned elements coming from different iterators is the 
+        same and equal to the length of the shortest iterator.
+    """
+    shortest = min([len(iterator) for iterator in args])
+    generators = [(item for item in iterator) for iterator in args]
+    for _ in range(shortest):
+        for gen in generators:
+            yield next(gen)
 
