7b8d3d4239e021d5c969cdcf38f43e51ac802381
TadBak <35119592+TadBak@users.noreply.github.com>
2018-03-16 15:30:27 +1100

    Add files via upload

---
+++ week11.py
@@ -0,0 +1,29 @@
+import sys
+
+class Tee:
+    """ Simple class for writing text data into several file-like objects.
+        None of the raised exceptions is caught in this class but passed to
+        the caller.
+    """
+
+    def __init__(self, *args):
+        self.outs = [fileobj for fileobj in args]
+
+    def write(self, str):
+        for fileobj in self.outs:
+            fileobj.write(str)
+
+    def writelines(self, str):
+        self.write(str + '\n')
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, type, value, traceback):
+        for fileobj in self.outs:
+            if fileobj is sys.stdout:
+                fileobj.flush()
+            else:
+                fileobj.close()
+
+
