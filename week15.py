72624ec24b9b05903d3527da6646eedbcc64e9b5
TadBak <35119592+TadBak@users.noreply.github.com>
2018-04-15 12:19:39 +1000

    Add files via upload
    Modified to use list comprehensions - inspired by Michael's solution

--- week15.py
+++ week15.py
@@ -3,10 +3,8 @@ from threading import Thread
 from queue import Queue
 
 def file_word_count(filename, q=None):
-    words = 0
     with open(filename, 'r') as input_file:
-        for line in input_file:
-            words += len(line.split())
+        words = sum(len(line.split()) for line in input_file)
     if q is None:
         return words
     else:
@@ -15,10 +13,7 @@ def file_word_count(filename, q=None):
 def count_words_sequential(pathname):
     """ Counting words -- sequential version
     """
-    total_words = 0
-    for text_file in glob.glob(pathname):
-        total_words += file_word_count(text_file)
-    return total_words
+    return sum(file_word_count(text_file) for text_file in glob.glob(pathname))
 
 def count_words_threaded(pathname):
     """ Counting words -- threaded version
@@ -32,7 +27,7 @@ def count_words_threaded(pathname):
         t.start()
     for t in threads:
         t.join()
-    while not fifo.empty():
+    while not fifo.empty(): # all threads finished, queue not changes anymore
         total_words += fifo.get()
     return total_words
 
